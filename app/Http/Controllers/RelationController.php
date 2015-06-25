b9933eaf92a71dd1beb5974cf44f597a2b0db5eb
jshi <shijian0602@hotmail.com>
2015-06-25 15:03:18 +1200

    refactor feedback to relation searchController exclude gna and hostile

---
+++ app/Http/Controllers/RelationController.php
@@ -0,0 +1,98 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Relation;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+
+
+class RelationController extends Controller {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+        $data = Input::except('_token');
+        foreach($data as $key => $value) {
+            $relation = Relation::firstOrNew(['elector_id' => $key]);
+            $value['user_id'] = Auth::user()->id;
+            $relation->fill($value);
+            $relation->save();
+        }
+        session()->flash('flash_message', 'Records have been updated!');
+        return redirect::back();
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
