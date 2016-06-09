9b37d0f1663f978f09956ba6bf18dcb2b8c6d4b4
jshi <shijian0602@hotmail.com>
2016-06-09 16:40:32 +1200

    bug fixed : rawQuery->door->groupBy from name to sortName

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -97,8 +97,8 @@ $router->resource('phone', 'PhoneController',
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
-
 
 //Event::listen('illuminate.query', function($query)
 //{
